d4066328a5d833bf9721ac6cded8a3175c4d143e
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2025-09-19 02:08:26 -0500

    se sube backend completo...

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 
 router.post('/register', usersController.register);
-router.post('/login', usersController.login); // For session endpoint if needed
+router.post('/login', usersController.login); 
 router.post('/recover-password', usersController.recoverPassword);
 router.get('/me/stats', usersController.getStats);
 
